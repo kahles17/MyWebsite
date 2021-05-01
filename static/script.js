2270b98fa7d1a60a7d2ae2186daa806a125370a9
Simon Roehrl <simon.roehrl@st.oth-regensburg.de>
2021-05-01 16:07:00 +0200

    halll

--- static/script.js
+++ static/script.js
@@ -1,4 +1,2 @@
-/*fetch('https://temperatur-api.herokuapp.com/simon')
-  .then(response => response.json())
-  .then(data => console.log(data));*/
+
 document.getElementById("number").innerHTML = 10;
