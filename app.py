acb5f5880f1521072b4fcdfc198ec4900aaf0e5a
Simon Roehrl <simon.roehrl@st.oth-regensburg.de>
2021-05-01 12:55:44 +0200

    Tech with Tim

--- app.py
+++ app.py
@@ -1,32 +1,11 @@
-# app.py
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_restfule import Api, Resource
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/getcalculation/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    A = request.args.get("A", None)
+class API(Resource):
+    def get(self):
+        return {"data":A}
 
-    response = {}
-    print(f"got name {name}")
-    # Check if user sent a name at all
-    if not A:
-        response["ERROR"] = "no parameter found, please send a parameter"
-    # Check if the user entered a number not a name
-    elif not str(name).isdigit():
-        response["ERROR"] = "name must be numeric."
-    # Now the user entered a valid name
-    else:
-        response["A"] = f"Hallo {A}"
-
-    # Return the response in json format
-    return jsonify(response)
-
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to Temperature Calculation !!</h1>"
-
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+api.add_resource(API,"/api/<int:A>")
