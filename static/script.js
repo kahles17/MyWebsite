6b0ff883dda4de879998219422c917597ecea4f2
Simon Roehrl <simon.roehrl@st.oth-regensburg.de>
2021-05-01 17:04:48 +0200

    Second

--- static/script.js
+++ static/script.js
@@ -1,2 +1,4 @@
-
+fetch('https://temperature-sensor-calculator.herokuapp.com/simon')
+  .then(response => response.json())
+  .then(data => console.log(data));
 document.getElementById("number").innerHTML = 10;
