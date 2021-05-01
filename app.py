aad5a398ad0517232fe64d4b7cc8b388778e78f7
Simon Roehrl <simon.roehrl@st.oth-regensburg.de>
2021-05-01 13:01:17 +0200

    Running Tech

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from flask_restfule import Api, Resource
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
 class API(Resource):
-    def get(self):
+    def get(self,A):
         return {"data":A}
 
 api.add_resource(API,"/api/<int:A>")
+if __name__ == "__main__":
+    app.run()
