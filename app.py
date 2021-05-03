68fd63c4cb9d8271f83deb46f1ead20090f2af76
Simon Roehrl <simon.roehrl@st.oth-regensburg.de>
2021-05-03 16:37:42 +0200

    First Sympy

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-from flask import Flask,render_template
-from flask_restful import Api, Resource
-
+from json import dumps
+from flask import Flask,render_template,request
+from sympy import symbols,Symbol,latex
+def add_to_output(latex,index,output):
+    output[str(index)]=latex
+def sympy_calc(output):
+    Untc,R10k,Rntc,Uref,Uadc= symbols("U_{NTC},R_{10k},R_{NTC},U_{ref},U_{ADC}")
+    dUadc = Symbol("\Delta U_{ADC}")
+    dRntc = Symbol("\Delta U_{NTC}")
+    dUref = Symbol("\Delta U_{ref}")
+    dR10k = Symbol("\Delta R_{10k}")
+    add_to_output(latex(Rntc),0,output)
+    add_to_output(latex(dUref),1,output)
+    add_to_output(latex(dR10k),2,output)
+def calc(a,b,c):
+    output = {}
+    sympy_calc(output)
+    return dumps(output)
 app = Flask(__name__)
 
 @app.route("/")
 def home():
    return render_template("index.html")
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}"
+@app.route("/api/")
+def calculation():
+    a = int(request.args.get("A"))
+    b = int(request.args.get("B"))
+    c = int(request.args.get("C"))
+    tex = calc(a,b,c)
+    return tex
 if __name__ == "__main__":
     app.run()
--- static/script.js
+++ static/script.js
@@ -1,13 +1,34 @@
-function Test(log)
-{
-    document.getElementById("number").innerHTML = log;
+var request = new XMLHttpRequest();
+function add_to_View(text)
+{ let obj = JSON.parse(text);
+  let count = Object.keys(obj).length;
+  let activeElement = DynamicMJ.TEX;
+  for (i = 0;i<count;i++)
+  {
+    activeElement.innerHTML = "\\["+obj[String(i)]+"\\]";
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub,activeElement]);
+    let newElement = document.createElement("div");
+    activeElement.parentNode.insertBefore(newElement , activeElement.nextSibling);
+    activeElement = newElement;
+  }
 }
 
-const Http = new XMLHttpRequest();
-const url='/simon';
-Http.open("GET", url);
-Http.send();
-answer =Http.responseText
-Http.onreadystatechange = (e) => {
-  Test(Http.responseText);
-}
+var DynamicMJ = {
+    TEX: document.getElementById("TEX"),
+  
+    update: function () {
+      let a = document.getElementById("A").value;
+      let b = document.getElementById("B").value;
+      let c = document.getElementById("C").value;
+      url='/api/?'+"A="+a+"&B="+b+"&C="+c;
+      request.open("GET", url);
+      request.addEventListener('load', function(event) {
+        if (request.status >= 200 && request.status < 300) {
+           add_to_View(request.responseText)
+        } else {
+           console.warn(request.statusText, request.responseText);
+        }
+     });
+      request.send();
+    }
+  };
