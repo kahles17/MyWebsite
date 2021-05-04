174f87ebd991f3b07d0ec7678029fb8682b9df7a
Simon Roehrl <simon.roehrl@st.oth-regensburg.de>
2021-05-04 09:35:56 +0200

    prettier page

--- static/script.js
+++ static/script.js
@@ -1,34 +1,40 @@
 var request = new XMLHttpRequest();
-function add_to_View(text)
-{ let obj = JSON.parse(text);
+var clicked = false;
+
+function add_to_View(text) {
+  let obj = JSON.parse(text);
   let count = Object.keys(obj).length;
   let activeElement = DynamicMJ.TEX;
-  for (i = 0;i<count;i++)
-  {
-    activeElement.innerHTML = "\\["+obj[String(i)]+"\\]";
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub,activeElement]);
-    let newElement = document.createElement("div");
-    activeElement.parentNode.insertBefore(newElement , activeElement.nextSibling);
-    activeElement = newElement;
+  for (i = 0; i < count; i++) {
+    activeElement.innerHTML = "\\[" + obj[String(i)] + "\\]";
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub, activeElement]);
+    if (i < (count - 1)) {
+      let newElement = document.createElement("div");
+      activeElement.parentNode.insertBefore(newElement, activeElement.nextSibling);
+      activeElement = newElement;
+    }
   }
 }
 
 var DynamicMJ = {
-    TEX: document.getElementById("TEX"),
-  
-    update: function () {
+  TEX: document.getElementById("TEX"),
+
+  update: function () {
+    if (!clicked) {
       let a = document.getElementById("A").value;
       let b = document.getElementById("B").value;
       let c = document.getElementById("C").value;
-      url='/api/?'+"A="+a+"&B="+b+"&C="+c;
+      url = '/api/?' + "A=" + a + "&B=" + b + "&C=" + c;
       request.open("GET", url);
-      request.addEventListener('load', function(event) {
+      request.addEventListener('load', function (event) {
         if (request.status >= 200 && request.status < 300) {
-           add_to_View(request.responseText)
+          add_to_View(request.responseText)
         } else {
-           console.warn(request.statusText, request.responseText);
+          console.warn(request.statusText, request.responseText);
         }
-     });
+      });
       request.send();
+      clicked = true;
     }
-  };
+  }
+};
