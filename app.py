7483a908f18fb537cbdb89517ac2f15e6e33eb87
kahles17 <70477581+kahles17@users.noreply.github.com>
2021-08-04 13:03:24 +0200

    Example Server

--- app.py
+++ app.py
@@ -1,32 +1,10 @@
-from json import dumps
-from flask import Flask,render_template,request
-from sympy import symbols,Symbol,latex
-def add_to_output(latex,index,output):
-    output[str(index)]=latex
-def sympy_calc(output):
-    Untc,R10k,Rntc,Uref,Uadc= symbols("U_{NTC},R_{10k},R_{NTC},U_{ref},U_{ADC}")
-    dUadc = Symbol("\Delta U_{ADC}")
-    dRntc = Symbol("\Delta U_{NTC}")
-    dUref = Symbol("\Delta U_{ref}")
-    dR10k = Symbol("\Delta R_{10k}")
-    add_to_output(latex(Rntc),0,output)
-    add_to_output(latex(dUref),1,output)
-    add_to_output(latex(dR10k),2,output)
-def calc(a,b,c):
-    output = {}
-    sympy_calc(output)
-    return dumps(output)
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-   return render_template("index.html")
-@app.route("/api/")
-def calculation():
-    a = int(request.args.get("A"))
-    b = int(request.args.get("B"))
-    c = int(request.args.get("C"))
-    tex = calc(a,b,c)
-    return tex
+@app.route('/', methods=['POST'])
+def form():
+    return print(request.data)
+
 if __name__ == "__main__":
     app.run()
