6f20e0d1b6da6df56511d582e8d31db41c8de025
kahles17 <70477581+kahles17@users.noreply.github.com>
2021-08-04 13:23:26 +0200

    Fixed Error

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask
 from flask_cors import CORS, cross_origin
-
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/', methods=['POST'])
+@cross_origin()
+def form():
+    return print(request.data)
 
 @app.route('/', methods=['POST'])
 def form():
